e8c3a76ab3e333a83b4c96b75939e95ffaca8539
Othniel Konan <kylekojey@gmail.com>
2016-03-19 01:00:46 +0200

    Implement readImages methods
    Split baseName into two part, a directory name
    and a file name

--- libs/volimage.cpp
+++ libs/volimage.cpp
@@ -5,37 +5,45 @@
  */
 
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <sstream>
 #include "volimage.h"
 
 
-Volimgae::VolImage(){
-
+VolImage::VolImage(){
+	width = 0;
+	height = 0;
 } // default constructor - define in .cpp
 
-Volimage::~VolImage(){
+VolImage::~VolImage(){
 
 } // destructor - define in .cpp file
 
 // populate the object with images in stack and
 //set member variables define in .cpp
-bool Volimage::readImages(std::string baseName) {
+bool VolImage::readImages(std::string baseName) {
+	using namespace std;
+	// baseName must have the form <directory>/<name>
+	string dir,name;
+	stringstream ss(baseName);
+	getline(ss,dir,'/');
+	getline(ss,name,'/');
 	return true;
 }
 
 // compute difference map and write out; define in .cpp
-void Volimage::diffmap(int sliceI, int sliceJ, std::string output_prefix) {
+void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix) {
 
 }
 
 // extract slice sliceId and write to output - define in .cpp
-void Volimage::extract(int sliceId, std::string output_prefix){
+void VolImage::extract(int sliceId, std::string output_prefix){
 
 }
 
 // number of bytes uses to store image data bytes
 //and pointers (ignore vector<> container, dims etc)
-int Volimage::volImageSize(void){
+int VolImage::volImageSize(void){
 
 }
