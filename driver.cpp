6fc4053ef6a35d69bdde0be73220541f48f25c5b
Othniel Konan <kylekojey@gmail.com>
2016-03-19 14:45:44 +0200

    Refactor code
    All slices are loaded at the start of program

--- driver.cpp
+++ driver.cpp
@@ -6,7 +6,6 @@
 
 #include <iostream>
 #include <sstream>
-#include "volimage.h"
 #include "libs/volimage.h"
 
 void clear();
@@ -15,11 +14,11 @@ int main(int argc, char * argv[]){
 	using namespace std;
 	string cmd_arg[4] = {"volimage", "-g", "-x", "-d"};
 	VolImage vol;
+	if( argc > 2) vol.readImages(string(argv[2]));
 	// Read argc from command line
 	switch(argc){
 		case 3:	// volimage <imageBase>
 			cout<<"volimage <imageBase>"<<endl;
-			vol.readImages(string(argv[2]));
 			if(string(argv[1])!=cmd_arg[0]) return 0;	// wrong argument name
 			break;
 		case 5:	// volimage <imageBase> [-g i]
@@ -35,7 +34,13 @@ int main(int argc, char * argv[]){
 		case 6:	// volimage <imageBase> [-x i output_file_name]
 			cout<<"volimage <imageBase> [-x i output_file_name]"<<endl;
 			if(string(argv[1])!=cmd_arg[0] || string(argv[3])!= cmd_arg[2]) return 0;
-			break;
+			{
+				cout << "in Here" << endl;
+				istringstream ss(argv[4]);
+				int i;
+				ss >> i;
+				vol.extract(i, string(argv[5]));
+			}break;
 		case 7:	// volimage <imageBase> [-d i j output_file_name]
 			cout<<"volimage <imageBase> [-d i j output_file_name]"<<endl;
 			if(string(argv[1])!=cmd_arg[0] || string(argv[3])!= cmd_arg[3]) return 0;
