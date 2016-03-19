56bb736739c464c6d757072fc7a9ff14cfe593b1
Othniel Konan <kylekojey@gmail.com>
2016-03-19 21:56:16 +0200

    Refactor code
    Add meaningful info for each method
    Remove dupplicate case

--- driver.cpp
+++ driver.cpp
@@ -11,17 +11,15 @@
 int main(int argc, char * argv[]){
 	using namespace std;
 	string cmd_arg[4] = {"volimage", "-g", "-x", "-d"};
+	string s;
 	VolImage vol;
 	if( argc > 2) vol.readImages(string(argv[2]));
 	// Read argc from command line
 	switch(argc){
 		case 3:
 			if(string(argv[1])!=cmd_arg[0]) return 0;	// wrong argument name
-			printInfo("volimage <imageBase>",vol.volImageSize());
-			break;
-		case 5:
-			if(string(argv[1])!=cmd_arg[0] || string(argv[3])!= cmd_arg[1]) return 0;	// Wrong argument names
-			printInfo("volimage <imageBase> [-g i]",vol.volImageSize());
+			s = "\nCommand: volimage <imageBase>\nDecription: Load data with base "+string(argv[2])+" to memory.";
+			printInfo(s,vol.volImageSize());
 			break;
 		case 6:
 			if(string(argv[1])!=cmd_arg[0] || ( string(argv[3])!= cmd_arg[2] && string(argv[3])!= cmd_arg[1]) ) return 0;
@@ -29,13 +27,15 @@ int main(int argc, char * argv[]){
 				istringstream ss(argv[4]);
 				int i;ss >> i;
 				vol.extract(i, string(argv[5]));
-				printInfo("volimage <imageBase> [-x i output_file_name]",vol.volImageSize());
+				s = "\nCommand: volimage <imageBase> [-x i output_file_name]\nDecription: Retrieve the slice with the index given and write this to an output sequence.";
+				printInfo(s,vol.volImageSize());
 			}
 			else if(string(argv[3])== cmd_arg[1]){
 				istringstream ss(argv[4]);
 				int row;ss >> row;
 				vol.g_extract(row, string(argv[5]));
-				printInfo("volimage <imageBase> [-g i output_file_name]",vol.volImageSize());
+				s = "\nCommand: volimage <imageBase> [-g i output_file_name]\nDecription: Extracts an image along row i of the volume, across all slices.";
+				printInfo(s,vol.volImageSize());
 			}
 			break;
 		case 7:
@@ -46,8 +46,9 @@ int main(int argc, char * argv[]){
 				ss >> i;
 				ss.clear();ss.str(argv[5]);ss >> j;
 				vol.diffmap(i,j, string(argv[6]));
+				s = "\nCommand: volimage <imageBase> [-d i j output_file_name]\nDecription: Computes the difference map between “slices” i and j.";
+				printInfo(s,vol.volImageSize());
 			}
-			printInfo("volimage <imageBase> [-d i j output_file_name]",vol.volImageSize());
 			break;
 		default:
 			cout<<"Wrong arguments"<<endl;
