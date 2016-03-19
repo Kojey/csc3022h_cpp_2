61541026191d877e1f537014da8dbf27e6e99e7a
Othniel Konan <kylekojey@gmail.com>
2016-03-19 15:36:39 +0200

    Refactor code

--- driver.cpp
+++ driver.cpp
@@ -24,26 +24,26 @@ int main(int argc, char * argv[]){
 		case 5:	// volimage <imageBase> [-g i]
 			cout<<"volimage <imageBase> [-g i]"<<endl;
 			if(string(argv[1])!=cmd_arg[0] || string(argv[3])!= cmd_arg[1]) return 0;	// Wrong argument names
-/*			{
-				istringstream ss(argv[4]);
-				int i;
-				ss >> i;
-				cout << "integer i = " << i << endl;
-			}*/
 			break;
 		case 6:	// volimage <imageBase> [-x i output_file_name]
 			cout<<"volimage <imageBase> [-x i output_file_name]"<<endl;
 			if(string(argv[1])!=cmd_arg[0] || string(argv[3])!= cmd_arg[2]) return 0;
 			{
-				cout << "in Here" << endl;
 				istringstream ss(argv[4]);
-				int i;
-				ss >> i;
+				int i;ss >> i;
 				vol.extract(i, string(argv[5]));
 			}break;
 		case 7:	// volimage <imageBase> [-d i j output_file_name]
 			cout<<"volimage <imageBase> [-d i j output_file_name]"<<endl;
 			if(string(argv[1])!=cmd_arg[0] || string(argv[3])!= cmd_arg[3]) return 0;
+			{
+				istringstream ss(argv[4]);
+				int i,j;
+				string _i,_j;
+				ss >> i; _i = ss.str();
+				ss.clear();ss.str(argv[5]);ss >> j; _j = ss.str();
+				vol.diffmap(i,j, string(argv[6]));
+			}
 			break;
 		default:
 			cout<<"default"<<endl;
